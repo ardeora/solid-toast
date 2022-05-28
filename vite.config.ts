458d8bae17b78863011ddb89df32d28ed5fce1b5
Aryan Deora <adeora@iu.edu>
2022-05-27 21:12:52 -0400

    Added d.ts types to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import { format, resolve } from 'path'
 import solid from 'vite-plugin-solid';
+import dts from 'vite-plugin-dts'
 import pkg from "./package.json";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -25,6 +27,7 @@ export default defineConfig({
     },
   },
   plugins: [
-    solid()
+    solid(),
+    dts()
   ]
 })
