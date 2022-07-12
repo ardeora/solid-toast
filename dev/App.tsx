9c80f7b003d60807c852054ef940de68e06b1dd9
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-07-12 16:20:46 +0800

    🎨 Improve debug page

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,18 +1,33 @@
 import { Component } from "solid-js";
 import toast, { Toaster } from "../src";
 
-const App: Component = () => (
-  <div>
-    <Toaster position="top-center" />
-    <button
-      onClick={(e) => {
-        e.preventDefault();
-        toast.success("Hello World!", { duration: Infinity });
-      }}
-    >
-      asf
-    </button>
-  </div>
-);
+const App: Component = () => {
+  let toastID: string[] = [];
+  const popSuccess = () =>
+    toastID.push(toast.success("Success!", { duration: Infinity }));
+  const popError = () =>
+    toastID.push(toast.error("Error!", { duration: Infinity }));
+  const closeAll = () => {
+    toastID.forEach((id) => toast.dismiss(id));
+    toastID = [];
+  };
+  return (
+    <div>
+      <Toaster position="top-center" />
+      <div
+        style={{
+          display: "flex",
+          "flex-direction": "column",
+          "align-items": "flex-start",
+          gap: "0.5rem",
+        }}
+      >
+        <button onClick={popSuccess}>Pop success</button>
+        <button onClick={popError}>Pop error</button>
+        <button onClick={closeAll}>Close all toasts</button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
