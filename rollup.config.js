3fe6e5d99225459876221aab293c0e0310c11d9c
Aryan Deora <adeora@iu.edu>
2022-07-12 00:20:08 -0400

    Version Bump and Docs Update

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 import withSolid from "rollup-preset-solid";
+import { terser } from "rollup-plugin-terser";
 
 export default withSolid({
   targets: ["esm", "cjs"],
+  plugins: [terser({
+    module: true,
+  })]
 });
