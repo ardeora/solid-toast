565ed9ad15f3ceeda22818a0dd136485b4f8f3fc
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-07-12 16:30:57 +0800

    🐛 Fixes #8

--- src/components/ErrorIcon.tsx
+++ src/components/ErrorIcon.tsx
@@ -1,19 +1,61 @@
-import { iconCircle, pingCircle, icon, infoDot } from "../util"
+import { iconCircle, pingCircle, icon, infoDot } from "../util";
 import { IconProps } from "../types";
 
 export const Error = (props: IconProps) => {
-
   const mainCircle = `${iconCircle} 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards`;
   const secondaryCircle = `${pingCircle} 1s cubic-bezier(0, 0, 0.2, 1) forwards`;
-  const infoDash = `${icon} 0.1s ease-in forwards`
-  const infoCircle = `${infoDot} 0.2s ease-out forwards`
+  const infoDash = `${icon} 0.1s ease-in forwards`;
+  const infoCircle = `${infoDot} 0.2s ease-out forwards`;
 
   return (
-    <svg style={{overflow: 'visible'}} viewBox="0 0 32 32">
-      <circle style={{animation: mainCircle, 'transform-origin': '50% 50%', 'animation-delay': '100ms', opacity: 0}} fill={props.primary || "#FF3B30"} cx="16" cy="16" r="16"/>
-      <circle style={{animation: secondaryCircle, 'transform-origin': '50% 50%', 'animation-delay': '320ms'}} fill={props.primary || "#FF3B30"} cx="16" cy="16" r="12"/>
-      <path style={{animation: infoDash, 'stroke-dasharray': 9, 'stroke-dashoffset': 9, 'animation-delay': '200ms'}} fill="none" stroke={props.secondary || "#FFFFFF"} stroke-width="4" stroke-linecap="round" d="M16,7l0,9"/>
-      <circle style={{animation: infoCircle, 'animation-delay': '320ms', opacity: 0}} fill={props.secondary || "#FFFFFF"}  cx="16" cy="16" r="2.5"/>
+    <svg style={{ overflow: "visible" }} viewBox="0 0 32 32" width="2rem" height="2rem">
+      <circle
+        style={{
+          animation: mainCircle,
+          "transform-origin": "50% 50%",
+          "animation-delay": "100ms",
+          opacity: 0,
+        }}
+        fill={props.primary || "#FF3B30"}
+        cx="16"
+        cy="16"
+        r="16"
+      />
+      <circle
+        style={{
+          animation: secondaryCircle,
+          "transform-origin": "50% 50%",
+          "animation-delay": "320ms",
+        }}
+        fill={props.primary || "#FF3B30"}
+        cx="16"
+        cy="16"
+        r="12"
+      />
+      <path
+        style={{
+          animation: infoDash,
+          "stroke-dasharray": 9,
+          "stroke-dashoffset": 9,
+          "animation-delay": "200ms",
+        }}
+        fill="none"
+        stroke={props.secondary || "#FFFFFF"}
+        stroke-width="4"
+        stroke-linecap="round"
+        d="M16,7l0,9"
+      />
+      <circle
+        style={{
+          animation: infoCircle,
+          "animation-delay": "320ms",
+          opacity: 0,
+        }}
+        fill={props.secondary || "#FFFFFF"}
+        cx="16"
+        cy="16"
+        r="2.5"
+      />
     </svg>
-  )
-}
+  );
+};
--- src/components/LoaderIcon.tsx
+++ src/components/LoaderIcon.tsx
@@ -1,12 +1,31 @@
-import { rotate } from "../util"
+import { rotate } from "../util";
 import { IconProps } from "../types";
 
 export const Loader = (props: IconProps) => {
-  const animation = `${rotate} 0.75s linear infinite`
+  const animation = `${rotate} 0.75s linear infinite`;
   return (
-    <svg style={{overflow: 'visible'}} viewBox="0 0 32 32">
-      <path fill="none" stroke={props.primary || "#E5E7EB"} stroke-width="4" stroke-miterlimit="10" d="M16,6c3,0,5.7,1.3,7.5,3.4c1.5,1.8,2.5,4,2.5,6.6c0,5.5-4.5,10-10,10S6,21.6,6,16S10.5,6,16,6z"/>
-      <path style={{animation, 'transform-origin': '50% 50%'}}  fill="none" stroke={props.secondary || "#4b5563"} stroke-width="4" stroke-linecap="round" stroke-miterlimit="10" d="M16,6c3,0,5.7,1.3,7.5,3.4c0.6,0.7,1.1,1.4,1.5,2.2"/>
+    <svg
+      style={{ overflow: "visible" }}
+      viewBox="0 0 32 32"
+      width="2rem"
+      height="2rem"
+    >
+      <path
+        fill="none"
+        stroke={props.primary || "#E5E7EB"}
+        stroke-width="4"
+        stroke-miterlimit="10"
+        d="M16,6c3,0,5.7,1.3,7.5,3.4c1.5,1.8,2.5,4,2.5,6.6c0,5.5-4.5,10-10,10S6,21.6,6,16S10.5,6,16,6z"
+      />
+      <path
+        style={{ animation, "transform-origin": "50% 50%" }}
+        fill="none"
+        stroke={props.secondary || "#4b5563"}
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-miterlimit="10"
+        d="M16,6c3,0,5.7,1.3,7.5,3.4c0.6,0.7,1.1,1.4,1.5,2.2"
+      />
     </svg>
-  )
-}
+  );
+};
