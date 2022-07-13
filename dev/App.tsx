230afd36548e85fab4142c266838a83f59d465f8
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-07-13 12:45:48 +0800

    Add default dimensions to SVG icons, Use margin instead of transform to set the position (#11)
    * Improve the development process
    
    * 🎨 Improve debug page
    
    * 🐛 Fixes #8
    
    * 🐛 Fixes #9
    
    * Update dev/App.tsx
    
    Co-authored-by: Aryan Deora <adeora@iu.edu>
    
    * Update src/components/ErrorIcon.tsx
    
    Co-authored-by: Aryan Deora <adeora@iu.edu>
    
    * Update src/components/LoaderIcon.tsx
    
    Co-authored-by: Aryan Deora <adeora@iu.edu>
    
    * Update src/components/SuccessIcon.tsx
    
    Co-authored-by: Aryan Deora <adeora@iu.edu>
    
    * 🎨 Move vite.config.ts to dev folder
    
    Co-authored-by: Aryan Deora <adeora@iu.edu>

---
+++ dev/App.tsx
@@ -0,0 +1,27 @@
+import { Component } from "solid-js";
+import toast, { Toaster } from "../src";
+
+const App: Component = () => {
+  const popSuccess = () => toast.success("Success!", { duration: Infinity });
+  const popError = () => toast.error("Error!", { duration: Infinity });
+  const closeAll = () => toast.dismiss();
+  return (
+    <div>
+      <Toaster position="top-center" />
+      <div
+        style={{
+          display: "flex",
+          "flex-direction": "column",
+          "align-items": "flex-start",
+          gap: "0.5rem",
+        }}
+      >
+        <button onClick={popSuccess}>Pop success</button>
+        <button onClick={popError}>Pop error</button>
+        <button onClick={closeAll}>Close all toasts</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/components/SuccessIcon.tsx
+++ src/components/SuccessIcon.tsx
@@ -1,17 +1,50 @@
-import { iconCircle, pingCircle, icon } from "../util"
+import { iconCircle, pingCircle, icon } from "../util";
 import { IconProps } from "../types";
 
 export const Success = (props: IconProps) => {
-
   const mainCircle = `${iconCircle} 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards`;
   const secondaryCircle = `${pingCircle} 1s cubic-bezier(0, 0, 0.2, 1) forwards`;
-  const check = `${icon} 0.2s ease-out forwards`
+  const check = `${icon} 0.2s ease-out forwards`;
 
   return (
-    <svg style={{overflow: 'visible'}} viewBox="0 0 32 32">
-      <circle style={{animation: mainCircle, 'transform-origin': '50% 50%', 'animation-delay': '100ms', opacity: 0}}  fill={props.primary || '#34C759'} cx="16" cy="16" r="16"/>
-      <circle style={{animation: secondaryCircle, 'transform-origin': '50% 50%', 'animation-delay': '250ms'}} fill={props.primary || '#34C759'} cx="16" cy="16" r="12"/>
-      <path style={{animation: check, 'stroke-dasharray': 22, 'stroke-dashoffset': 22, 'animation-delay': '250ms'}} fill="none" stroke={props.secondary || '#FCFCFC'} stroke-width="4" stroke-linecap="round" stroke-miterlimit="10" d="M9.8,17.2l3.8,3.6c0.1,0.1,0.3,0.1,0.4,0l9.6-9.7"/>
+    <svg style={{ overflow: "visible" }} viewBox="0 0 32 32" width="1.25rem" height="1.25rem">
+      <circle
+        style={{
+          animation: mainCircle,
+          "transform-origin": "50% 50%",
+          "animation-delay": "100ms",
+          opacity: 0,
+        }}
+        fill={props.primary || "#34C759"}
+        cx="16"
+        cy="16"
+        r="16"
+      />
+      <circle
+        style={{
+          animation: secondaryCircle,
+          "transform-origin": "50% 50%",
+          "animation-delay": "250ms",
+        }}
+        fill={props.primary || "#34C759"}
+        cx="16"
+        cy="16"
+        r="12"
+      />
+      <path
+        style={{
+          animation: check,
+          "stroke-dasharray": 22,
+          "stroke-dashoffset": 22,
+          "animation-delay": "250ms",
+        }}
+        fill="none"
+        stroke={props.secondary || "#FCFCFC"}
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-miterlimit="10"
+        d="M9.8,17.2l3.8,3.6c0.1,0.1,0.3,0.1,0.4,0l9.6-9.7"
+      />
     </svg>
-  )
-}
+  );
+};
