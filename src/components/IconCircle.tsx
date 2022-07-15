835cbb467178ae8c0718faf461da2417650c2d6f
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-07-15 11:22:24 +0800

    Update src/components/IconCircle.tsx
    Co-authored-by: Aryan Deora <adeora@iu.edu>

--- src/components/IconCircle.tsx
+++ src/components/IconCircle.tsx
@@ -4,15 +4,17 @@ import { genSVGCubicBezier } from '../util';
 
 export const MainCircle: Component<{ fill: string }> = (props) => {
   const publicProps: JSX.AnimateSVGAttributes<SVGAnimateElement> = {
-    dur: '0.3s',
+    dur: '0.35s',
     begin: '100ms',
     fill: 'freeze',
-    ...genSVGCubicBezier('0.137 0.69 0.25 1'),
+    calcMode: 'spline',
+    keyTimes: '0; 0.6; 1',
+    keySplines: '0.25 0.71 0.4 0.88; .59 .22 .87 .63',
   };
   return (
-    <circle fill={props.fill} cx="16" cy="16" r="16">
-      <animate attributeName="opacity" values="0;1" {...publicProps} />
-      <animate attributeName="r" values="0;16" {...publicProps} />
+    <circle fill={props.fill} cx="16" cy="16" r="0">
+      <animate attributeName="opacity" values="0; 1; 1" {...publicProps} />
+      <animate attributeName="r" values="0; 17.5; 16" {...publicProps} />
     </circle>
   );
 };
